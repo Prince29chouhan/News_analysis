3812ee3eb647447dd0f3fa8c56a5af29177b3b79
Prince Chouhan <princechouhan98765@gmail.com>
2025-03-19 00:09:54 +0530

    first commit

---
+++ api.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from utils import fetch_news, analyze_sentiment
+
+app = FastAPI()
+
+class NewsRequest(BaseModel):
+    company: str
+
+@app.post("/get_news/")
+def get_news(data: NewsRequest):
+    news_articles = fetch_news(data.company)
+    for article in news_articles:
+        article["sentiment"] = analyze_sentiment(article["summary"])
+    
+    return {"articles": news_articles}
