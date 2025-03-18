a41acb653eabb18e563193563b78b6b915e9f31a
Prince Chouhan <princechouhan98765@gmail.com>
2025-03-19 02:04:03 +0530

    updated

--- utils.py
+++ utils.py
@@ -9,13 +9,6 @@ from deep_translator import GoogleTranslator
 import gtts
 import os
 
-from transformers import AutoTokenizer
-from transformers import AutoModelForSequenceClassification
-from scipy.special import softmax
-
-MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
 
 nltk.download('vader_lexicon')
